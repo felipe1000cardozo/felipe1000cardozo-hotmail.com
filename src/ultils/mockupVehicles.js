deeada96155b5f9aadd55224c77a2b0116bac6fc
felipe1000cardozo <37625101+felipe1000cardozo@users.noreply.github.com>
2020-03-31 17:08:27 -0300

    Created vehicles mockup

---
+++ src/ultils/mockupVehicles.js
@@ -0,0 +1,196 @@
+const vehicles = [
+  {
+    id: 1,
+    model: "ybr",
+    brand: "yamaha",
+    price: 11000,
+    year: 2015,
+    km: 40000,
+    power: 125,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio maxime mollitia deleniti. Fugit autem non, explicabo facilis error architecto ducimus consequuntur soluta nobis harum libero debitis unde dignissimos ex hic!",
+    imgs: [
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-1-1585615463-v1.jpg",
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-3-1585615469-v1.jpg",
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-2-1585615466-v1.jpg"
+    ]
+  },
+  {
+    id: 2,
+    model: "ybr",
+    brand: "yamaha",
+    price: 11000,
+    year: 2015,
+    km: 40000,
+    power: 125,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio maxime mollitia deleniti. Fugit autem non, explicabo facilis error architecto ducimus consequuntur soluta nobis harum libero debitis unde dignissimos ex hic!",
+    imgs: [
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-1-1585615463-v1.jpg",
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-3-1585615469-v1.jpg",
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-2-1585615466-v1.jpg"
+    ]
+  },
+  {
+    id: 3,
+    model: "ybr",
+    brand: "yamaha",
+    price: 11000,
+    year: 2015,
+    km: 40000,
+    power: 125,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio maxime mollitia deleniti. Fugit autem non, explicabo facilis error architecto ducimus consequuntur soluta nobis harum libero debitis unde dignissimos ex hic!",
+    imgs: [
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-1-1585615463-v1.jpg",
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-3-1585615469-v1.jpg",
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-2-1585615466-v1.jpg"
+    ]
+  },
+  {
+    id: 4,
+    model: "ybr",
+    brand: "yamaha",
+    price: 11000,
+    year: 2015,
+    km: 40000,
+    power: 125,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio maxime mollitia deleniti. Fugit autem non, explicabo facilis error architecto ducimus consequuntur soluta nobis harum libero debitis unde dignissimos ex hic!",
+    imgs: [
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-1-1585615463-v1.jpg",
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-3-1585615469-v1.jpg",
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-2-1585615466-v1.jpg"
+    ]
+  },
+  {
+    id: 5,
+    model: "ybr",
+    brand: "yamaha",
+    price: 11000,
+    year: 2015,
+    km: 40000,
+    power: 125,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio maxime mollitia deleniti. Fugit autem non, explicabo facilis error architecto ducimus consequuntur soluta nobis harum libero debitis unde dignissimos ex hic!",
+    imgs: [
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-1-1585615463-v1.jpg",
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-3-1585615469-v1.jpg",
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-2-1585615466-v1.jpg"
+    ]
+  },
+  {
+    id: 6,
+    model: "ybr",
+    brand: "yamaha",
+    price: 11000,
+    year: 2015,
+    km: 40000,
+    power: 125,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio maxime mollitia deleniti. Fugit autem non, explicabo facilis error architecto ducimus consequuntur soluta nobis harum libero debitis unde dignissimos ex hic!",
+    imgs: [
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-1-1585615463-v1.jpg",
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-3-1585615469-v1.jpg",
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-2-1585615466-v1.jpg"
+    ]
+  },
+  {
+    id: 7,
+    model: "ybr",
+    brand: "yamaha",
+    price: 11000,
+    year: 2015,
+    km: 40000,
+    power: 125,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio maxime mollitia deleniti. Fugit autem non, explicabo facilis error architecto ducimus consequuntur soluta nobis harum libero debitis unde dignissimos ex hic!",
+    imgs: [
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-1-1585615463-v1.jpg",
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-3-1585615469-v1.jpg",
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-2-1585615466-v1.jpg"
+    ]
+  },
+  {
+    id: 8,
+    model: "ybr",
+    brand: "yamaha",
+    price: 11000,
+    year: 2015,
+    km: 40000,
+    power: 125,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio maxime mollitia deleniti. Fugit autem non, explicabo facilis error architecto ducimus consequuntur soluta nobis harum libero debitis unde dignissimos ex hic!",
+    imgs: [
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-1-1585615463-v1.jpg",
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-3-1585615469-v1.jpg",
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-2-1585615466-v1.jpg"
+    ]
+  },
+  {
+    id: 9,
+    model: "ybr",
+    brand: "yamaha",
+    price: 11000,
+    year: 2015,
+    km: 40000,
+    power: 125,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio maxime mollitia deleniti. Fugit autem non, explicabo facilis error architecto ducimus consequuntur soluta nobis harum libero debitis unde dignissimos ex hic!",
+    imgs: [
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-1-1585615463-v1.jpg",
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-3-1585615469-v1.jpg",
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-2-1585615466-v1.jpg"
+    ]
+  },
+  {
+    id: 10,
+    model: "ybr",
+    brand: "yamaha",
+    price: 11000,
+    year: 2015,
+    km: 40000,
+    power: 125,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio maxime mollitia deleniti. Fugit autem non, explicabo facilis error architecto ducimus consequuntur soluta nobis harum libero debitis unde dignissimos ex hic!",
+    imgs: [
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-1-1585615463-v1.jpg",
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-3-1585615469-v1.jpg",
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-2-1585615466-v1.jpg"
+    ]
+  },
+  {
+    id: 11,
+    model: "ybr",
+    brand: "yamaha",
+    price: 11000,
+    year: 2015,
+    km: 40000,
+    power: 125,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio maxime mollitia deleniti. Fugit autem non, explicabo facilis error architecto ducimus consequuntur soluta nobis harum libero debitis unde dignissimos ex hic!",
+    imgs: [
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-1-1585615463-v1.jpg",
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-3-1585615469-v1.jpg",
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-2-1585615466-v1.jpg"
+    ]
+  },
+  {
+    id: 12,
+    model: "ybr",
+    brand: "yamaha",
+    price: 11000,
+    year: 2015,
+    km: 40000,
+    power: 125,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio maxime mollitia deleniti. Fugit autem non, explicabo facilis error architecto ducimus consequuntur soluta nobis harum libero debitis unde dignissimos ex hic!",
+    imgs: [
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-1-1585615463-v1.jpg",
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-3-1585615469-v1.jpg",
+      "https://www.moto.com.br/img/2020/03/30/yamaha_ybr_125_ed-2018-2-1585615466-v1.jpg"
+    ]
+  }
+];
+
+export default vehicles;
