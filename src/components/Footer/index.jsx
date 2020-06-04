46ba2628c64e7057f28640eac3d44df726cd4d36
Anderson Vieira Lidio <andersonvlidio@gmail.com>
2020-06-04 18:03:03 -0300

    update footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Foot, Path, CompanyInfo } from "./styles";
+import { Foot } from "./styles";
 import { Fragment } from "react";
 import { useHistory } from "react-router-dom";
+import { RiFacebookCircleLine } from "react-icons/ri";
+import { FiInstagram } from "react-icons/fi";
 
 const Footer = () => {
   const history = useHistory();
@@ -10,13 +12,36 @@ const Footer = () => {
     <Fragment>
       {pathname === "/admin" || pathname === "/login" ? null : (
         <Foot>
-          <Path>
-            <h4>Vehicles shop</h4>
-          </Path>
-          <CompanyInfo>
-            <p>Todos os direitos Reservados </p>
-            <p>A&F Desenvolvimento</p>
-          </CompanyInfo>
+          <div className="path">
+            <div>
+              <h4>Atendimento</h4>
+              <p>
+                Segunda a Sexta: 08:00 – 19:00 <br />
+                Sábado: 08:00 – 18:00 <br />
+                Domingo e Feriados: Fechado
+              </p>
+            </div>
+            <div>
+              <h4>Endereço:</h4>
+              <p>
+                Rua Francisno nappi, 1419
+                <br />
+                Bairro ipiranga, São José – SC <br />
+                Cep:88111490
+              </p>
+            </div>
+          </div>
+          <div className="company-info">
+            <h4>Redes Sociais:</h4>
+            <div>
+              <a href="https://www.facebook.com/" target="blank">
+                <RiFacebookCircleLine size="40" color="#fff" />
+              </a>
+              <a href="https://www.instagram.com/" target="blank">
+                <FiInstagram size="40" color="#fff" />
+              </a>
+            </div>
+          </div>
         </Foot>
       )}
     </Fragment>
--- src/components/Footer/styles.jsx
+++ src/components/Footer/styles.jsx
@@ -4,34 +4,89 @@ export const Foot = styled.div`
   display: flex;
   max-width: 1114px;
   height: 110px;
-  color: var(--four-color);
-`;
-export const Path = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 50%;
-  background-color: var(--primary-color);
-  position: relative;
-  &:after {
-    content: "";
-    height: 100%;
-    width: 130px;
-    background-color: inherit;
-    right: -65px;
-    position: absolute;
-    z-index: 1;
-    transform: skewX(230deg);
-  }
-`;
-
-export const CompanyInfo = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  padding-right: 20px;
-  justify-content: center;
-  width: 50%;
   background-color: var(--secoundary-color);
-  box-shadow: inset 0px 4px 4px var(--primary-color);
+  color: var(--four-color);
+  .adress {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    width: 50%;
+    background-color: var(--primary-color);
+
+    z-index: 1;
+  }
+  .path {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 50%;
+    background-color: var(--primary-color);
+    border-bottom-right-radius: 55px;
+    border-top-right-radius: 55px;
+    position: relative;
+    z-index: 1;
+    div {
+      justify-content: center;
+      display: flex;
+      flex-direction: column;
+      padding: 30px 15px 30px 30px;
+      h4 {
+        padding-bottom: 5px;
+        font-weight: 400;
+        font-size: 16px;
+      }
+      p {
+        font-size: 12px;
+        color: ;
+      }
+    }
+  }
+  .company-info {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    padding-right: 20px;
+    justify-content: center;
+    width: 50%;
+    background-color: var(--secoundary-color);
+    z-index: 1;
+    padding: 10px;
+    h4 {
+      padding-bottom: 15px;
+      font-weight: 400;
+      font-size: 16px;
+    }
+  }
+
+  /* ::before {
+    content: "";
+    width: 40vw;
+    background-color: var(--primary-color);
+    height: 110px;
+    position: absolute;
+    z-index: 0;
+    left: 0;
+  }
+  ::after {
+    content: "";
+    width: 40vw;
+    background-color: var(--secoundary-color);
+    height: 110px;
+    position: absolute;
+    z-index: 0;
+    right: 0;
+  } */
+
+  @media (max-width: 768px) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .path {
+      display: none;
+    }
+    .company-info {
+      align-items: center;
+      justify-content: center;
+    }
+  }
 `;
